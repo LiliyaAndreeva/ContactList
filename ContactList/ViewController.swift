940fcd848d7c61aa8ed711346d94b41239dd0ae1
Liliya Andreeva <Liliya_yanbaeva@mail.ru>
2023-10-25 11:38:38 +0500

    Initial Commit

---
+++ ContactList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactList
+//
+//  Created by Лилия Андреева on 25.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
